6ad71c722816a591d2e0a886a08cd042ee7762ee
JohanMaxcorp <johan.maxcorp@gmail.com>
2017-03-01 00:52:48 -0500

    babelify and browserify

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ var express = require('express');
 
 var app = express();
 
+
+app.set('view engine', 'jade');
+
+app.use(express.static('public'));
+
 app.get('/' , function (req , res) {
-   res.send('Hola Mundo');
+   res.render('index');
 });
 
-app.listen(3000,function(err){
+app.listen(3001,function(err){
     if(err) return console.log('Hubo un error'), process.exit(1);
     console.log('Devgran escuchando en el puerto 3000');
 }); 
---
+++ src/index.js
@@ -0,0 +1,3 @@
+var numeros = [1 ,2 ,3 ,4 ,5];
+
+numeros.map( n => console.log( n+1) );
